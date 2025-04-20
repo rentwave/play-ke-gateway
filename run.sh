73ca2764e9c01b83de355cd72c1f2efeec2603f7
Osiru <marikwamari023@gmail.com>
2025-04-20 23:04:16 +0400

    deploy new formats

--- run.sh
+++ run.sh
@@ -6,6 +6,4 @@ exec gunicorn \
     --bind 0.0.0.0:$PORT \
     --access-logfile - \
     --timeout 3600 \
-    --worker-class gevent \
-    --workers 4\
-    gateway.wsgi:application
+    gateway.wsgi:application -w 2
